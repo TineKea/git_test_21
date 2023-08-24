695ee597de197d36c94b67fc89a5428e3280aad8
Tine <titi0001@stud.kea.dk>
2023-08-24 11:50:05 +0200

    js noter

---
+++ test1.js
@@ -0,0 +1,8 @@
+let greeting = "Hello World";
+console.log(greeting); 
+
+var tekst = 'mit navn er';
+tekst = tekst + ' tine';
+console.log (tekst);
+//var = varibael mens tekst er navnet for var og 'xxxx' er indholdet i varibalen
+
