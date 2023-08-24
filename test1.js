194b629d932bb3016b1d8ad769dc1431159f31f3
Tine <titi0001@stud.kea.dk>
2023-08-24 11:54:40 +0200

    noter til js

--- test1.js
+++ test1.js
@@ -6,3 +6,5 @@ tekst = tekst + ' tine';
 console.log (tekst);
 //var = varibael mens tekst er navnet for var og 'xxxx' er indholdet i varibalen
 
+//console.log er for at fremkalde en konsolen i browersen. 
+
